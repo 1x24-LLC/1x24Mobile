9038cd5ef690f004423d27386dc42ad18bc15e92
Chris Wilson <chris@ocmit.com>
2024-03-21 23:05:07 -0700

    Updated quote style in import statements
    Updated the import statement in `apiService.js` and the assignment in `config.js` to use double quotes instead of single quotes for `API_BASE_URL`. This is a stylistic change to maintain consistency across the codebase. No functional changes were made.

--- 1x24mobile/src/config.js
+++ 1x24mobile/src/config.js
@@ -1,17 +1,9 @@
 let API_BASE_URL;
 
-if (process.env.NODE_ENV === 'production')
-{
-	API_BASE_URL = "https://api.1x24.com"; 
-} else
-{
-	API_BASE_URL = "https://1x24-test.azurewebsites.net/"; 
+if (process.env.NODE_ENV === "production") {
+  API_BASE_URL = "https://api.1x24.com";
+} else {
+  API_BASE_URL = "https://1x24-test.azurewebsites.net/";
 }
-
-
-
-
-
-
 
 export { API_BASE_URL };
