df133d6423a13f0edb59c26e98f1639a4db1b451
Chris Wilson <chris@ocmit.com>
2024-03-21 22:40:19 -0700

    Commit: Added linting, formatting, and new dependencies; updated import styles
    This commit includes several significant changes. Linting and formatting have been added to the pre-commit process with the addition of `npx lint-staged` and updates to `.prettierignore` and `.prettierrc`. Stylistic changes have been made in `App.js` to use double quotes for import statements and certain properties. The `app.json` file has been updated to include all assets in the standalone app bundle. The Babel configuration has been modified in `babel.config.js` to include new presets and plugins.
    
    Several new dependencies and devDependencies have been added to `package-lock.json` and `package.json`, including libraries for HTTP requests, UI design, handling safe area insets, and code formatting, as well as tools for preventing bad git commits, running linters on staged files, and more.
    
    Two new files, `apiService.js` and `config.js`, have been created to handle API requests and configuration respectively. The `apiService.js` file includes functions for setting, removing, and getting an authorization token, as well as making GET, POST, and DELETE requests. The `config.js` file exports a constant `API_BASE_URL` which is set to different values depending on the environment.

---
+++ 1x24mobile/src/api/apiService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { API_BASE_URL } from '/src/config';
+
+const apiService = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const setAuthToken = (token) => {
+  apiService.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
+
+export const removeAuthToken = () => {
+  delete apiService.defaults.headers.common["Authorization"];
+};
+
+export const getAuthToken = () => {
+  return apiService.defaults.headers.common["Authorization"];
+};
+
+
+
+
+export const apiGet = async (url) => {
+  try {
+    const response = await apiService.get(url);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const apiPost = async (url, body) => {
+  try {
+    const response = await apiService.post(url, body);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const apiDel = async (url) => {
+  try {
+    const response = await apiService.delete(url);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
