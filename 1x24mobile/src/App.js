ca85f3e2b7de17a9287a53e1db61efa1cc541e75
Chris Wilson <chris@ocmit.com>
2024-03-23 15:22:33 -0700

    Adding initial theming support

--- 1x24mobile/src/App.js
+++ 1x24mobile/src/App.js
@@ -1,63 +1,13 @@
 import * as React from "react";
 import { Image, View, StyleSheet, Touchable } from "react-native";
-import { Appbar, Badge, IconButton } from "react-native-paper";
+import { GlobalStyles } from "./ui/styles/globalStyles";
+import "./ui/components/appHeader";
+import AppHeader from "./ui/components/appHeader";
 
 const App = () => (
-    <Appbar.Header>
-        <IconButton
-            icon={({ size }) => (
-                <Image
-                    source={require('./../assets/images/logo32.png')}
-                    style={{ width: 66, height: 32 }}
-                />
-            )}
-            size={66}
-            onPress={() =>
-            {
-                alert("Main menu!");
-            }}
-        />
-        <Appbar.Action
-      icon="camera"
-      onPress={() => {
-        alert("Add photo!");
-      }}
-    />
-    <Appbar.Content title="" subtitle="" />
-
-    <View style={styles.iconContainer}>
-      <IconButton
-        icon="message-outline"
-        onPress={() => {
-          alert("Messages!");
-        }}
-      />
-      <Badge style={styles.badge}>420</Badge>
-    </View>
-
-    <Appbar.Action
-      icon="login"
-      onPress={() => {
-        alert("Login!");
-      }}
-    />
-  </Appbar.Header>
+  <View style={ GlobalStyles.appContainer }>
+    <AppHeader />
+  </View>
 );
-
-const styles = StyleSheet.create({
-  iconContainer: {
-    flexDirection: "row", // to align IconButton and Badge in one line
-    justifyContent: "center", // to center them on the main axis
-    alignItems: "center", // to center them on cross-axis
-    //padding: 10, // to give some space around
-  },
-  badge: {
-    backgroundColor: "red", // to make the badge pop out
-    color: "white", // to make the text color white
-    marginLeft: -25, // to give some space between the IconButton and Badge
-    top: -25, // to move the Badge a bit higher
-    //marginTop: 50, // to give some space between the IconButton and Badge
-  },
-});
 
 export default App;
