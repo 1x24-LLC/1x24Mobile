ebdaf4e0b3bc6be1994e8011087a6fa0766082c9
Chris Wilson <chris@ocmit.com>
2024-03-23 15:22:33 -0700

    Adding initial theming support

---
+++ 1x24mobile/src/ui/components/appHeader.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Image, View, StyleSheet, Touchable } from "react-native";
+import { Appbar, Badge, IconButton } from "react-native-paper";
+import { GlobalStyles } from "./../styles/globalStyles";
+
+const AppHeader = () => (
+  <Appbar.Header>
+    <IconButton
+      icon={({ size }) => (
+        <Image
+          source={require("./../../../assets/images/logo32.png")}
+          style={ GlobalStyles.AppHeader.logoContainer }
+        />
+      )}
+      size={66}
+      onPress={() => {
+        alert("Main menu!");
+      }}
+    />
+    <Appbar.Action
+      icon="camera"
+      onPress={() => {
+        alert("Add photo!");
+      }}
+    />
+    <Appbar.Content title="" subtitle="" />
+
+    <View style={GlobalStyles.AppHeader.iconContainer}>
+      <IconButton
+        icon="message-outline"
+        onPress={() => {
+          alert("Messages!");
+        }}
+      />
+      <Badge style={GlobalStyles.AppHeader.badge}>420</Badge>
+    </View>
+
+    <Appbar.Action
+      icon="login"
+      onPress={() => {
+        alert("Login!");
+      }}
+    />
+  </Appbar.Header>
+);
+
+export default AppHeader;
