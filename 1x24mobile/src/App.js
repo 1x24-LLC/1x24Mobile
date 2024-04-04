6350eea3e7279d3eae2eebfac62e33b7e7805cab
Chris Wilson <chris@ocmit.com>
2024-04-04 13:48:57 -0700

    Updated code formatting and style across files
    Updated the `.prettierrc` file to include specific formatting rules. Applied these rules across multiple files including `App.js`, `app.json`, `site.webmanifest`, `babel.config.js`, `eas.json`, `package.json`, `config.js`, `appHeader.js`, `feed.js`, `feedCard.js`, `feedCardMenu.js`, and others. Changes are purely stylistic, involving the replacement of double quotes with single quotes, increasing indentation for better readability, and reformatting certain object definitions. No changes to functionality were made.

--- 1x24mobile/src/App.js
+++ 1x24mobile/src/App.js
@@ -1,26 +1,26 @@
-import * as React from "react";
-import { Image, View, SafeAreaView, Text } from "react-native";
+import * as React from 'react';
+import { Image, View, SafeAreaView, Text } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
-import { GlobalStyles } from "./ui/styles/globalStyles";
-import AppHeader from "./ui/components/appHeader";
-import Feed from "./ui/components/feed";
+    SafeAreaProvider,
+    useSafeAreaInsets
+} from 'react-native-safe-area-context';
+import { GlobalStyles } from './ui/styles/globalStyles';
+import AppHeader from './ui/components/appHeader';
+import Feed from './ui/components/feed';
 
 const App = () => {
-  const insets = useSafeAreaInsets();
+    const insets = useSafeAreaInsets();
 
-  return (
-    <View style={GlobalStyles.appContainer}>
-      <SafeAreaView
-        style={(GlobalStyles.appContainer, { paddingTop: insets.top })}
-      >
-        <AppHeader />
-      </SafeAreaView>
-      <Feed />
-    </View>
-  );
+    return (
+        <View style={GlobalStyles.appContainer}>
+            <SafeAreaView
+                style={(GlobalStyles.appContainer, { paddingTop: insets.top })}
+            >
+                <AppHeader />
+            </SafeAreaView>
+            <Feed />
+        </View>
+    );
 };
 
 export default App;
--- 1x24mobile/src/ui/components/feedCard.js
+++ 1x24mobile/src/ui/components/feedCard.js
@@ -1,136 +1,148 @@
-import React, { useState } from "react";
-import { View, Image, Text, ScrollView } from "react-native";
-import { GlobalStyles } from "./../styles/globalStyles";
-import FeedCardMenu from "./feedCardMenu";
+import React, { useState } from 'react';
+import { View, Image, Text, ScrollView } from 'react-native';
+import { GlobalStyles } from './../styles/globalStyles';
+import FeedCardMenu from './feedCardMenu';
 
 const FeedCard = () => {
-  const [layout, setLayout] = useState({
-    width: 0,
-    height: 0,
-    maxHeight: 0,
-    textHeight: 0,
-  });
+    const [layout, setLayout] = useState({
+        width: 0,
+        height: 0,
+        maxHeight: 0,
+        textHeight: 0
+    });
 
-  const [titleMenuLayout, setTitleMenuLayout] = useState({
-    height: 0,
-  });
+    const [titleMenuLayout, setTitleMenuLayout] = useState({
+        height: 0
+    });
 
-  let imgWidth = Math.floor(Math.random() * (800 - 300 + 1) + 300);
-  let imgHeight = Math.floor(Math.random() * (800 - 300 + 1) + 300);
-  let imgUri = "https://picsum.photos/" + imgHeight + "/" + imgWidth;
-  console.log("Loading: " + imgUri);
+    let imgWidth = Math.floor(Math.random() * (800 - 300 + 1) + 300);
+    let imgHeight = Math.floor(Math.random() * (800 - 300 + 1) + 300);
+    let imgUri = 'https://picsum.photos/' + imgHeight + '/' + imgWidth;
+    console.log('Loading: ' + imgUri);
 
-  return (
-    <View
-      style={GlobalStyles.Card.container}
-      onLayout={(event) => {
-        const { width, height } = event.nativeEvent.layout;
-        const maxHeight = height * 0.85;
-        const textHeight = height - maxHeight;
-        setLayout({ width, height, maxHeight, textHeight });
-      }}
-    >
-      <Image
-        style={{
-          flex: 1,
-          width: undefined,
-          maxHeight: layout.maxHeight,
-          height: undefined,
-        }}
-        source={{
-          uri: imgUri,
-        }}
-        resizeMode="contain"
-      />
-      <View
-        onLayout={(event) => {
-          const { width, height } = event.nativeEvent.layout;
-          setTitleMenuLayout({ height });
-        }}
-      >
-        <Text style={GlobalStyles.Card.title}>Title!</Text>
-        <FeedCardMenu />
-      </View>
-      <ScrollView style={{ flex: 1 }}>
-        <Text style={GlobalStyles.Card.description}>
-          Hello! Yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada yada yada yada yada yada yada yada yada yada yada yada yada yada
-          yada{" "}
-        </Text>
-      </ScrollView>
-    </View>
-  );
+    return (
+        <View
+            style={GlobalStyles.Card.container}
+            onLayout={(event) => {
+                const { width, height } = event.nativeEvent.layout;
+                const maxHeight = height * 0.85;
+                const textHeight = height - maxHeight;
+                setLayout({ width, height, maxHeight, textHeight });
+            }}
+        >
+            <Image
+                style={{
+                    flex: 1,
+                    width: undefined,
+                    maxHeight: layout.maxHeight,
+                    height: undefined
+                }}
+                source={{
+                    uri: imgUri
+                }}
+                resizeMode="contain"
+            />
+            <View
+                onLayout={(event) => {
+                    const { width, height } = event.nativeEvent.layout;
+                    setTitleMenuLayout({ height });
+                }}
+            >
+                <Text style={GlobalStyles.Card.title}>Title!</Text>
+                <FeedCardMenu />
+            </View>
+            <ScrollView style={{ flex: 1 }}>
+                <Text style={GlobalStyles.Card.description}>
+                    Hello! Yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada yada yada yada yada yada yada yada yada yada
+                    yada yada yada{' '}
+                </Text>
+            </ScrollView>
+        </View>
+    );
 };
 
 export default FeedCard;
