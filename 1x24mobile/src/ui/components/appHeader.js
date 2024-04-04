81cdb271631d11b3a5d6718c9dcf5de5e2685128
Chris Wilson <chris@ocmit.com>
2024-04-04 13:48:57 -0700

    Updated code formatting and style across files
    Updated the `.prettierrc` file to include specific formatting rules. Applied these rules across multiple files including `App.js`, `app.json`, `site.webmanifest`, `babel.config.js`, `eas.json`, `package.json`, `config.js`, `appHeader.js`, `feed.js`, `feedCard.js`, `feedCardMenu.js`, and others. Changes are purely stylistic, involving the replacement of double quotes with single quotes, increasing indentation for better readability, and reformatting certain object definitions. No changes to functionality were made.

--- 1x24mobile/src/ui/components/appHeader.js
+++ 1x24mobile/src/ui/components/appHeader.js
@@ -1,35 +1,38 @@
-import * as React from "react";
-import { Image, View, StyleSheet, TouchableOpacity } from "react-native";
-import { Appbar, Badge, IconButton } from "react-native-paper";
-import IconFontAwesome from "react-native-vector-icons/FontAwesome";
-import IconMaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { GlobalStyles } from "./../styles/globalStyles";
+import * as React from 'react';
+import { Image, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Appbar, Badge, IconButton } from 'react-native-paper';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { GlobalStyles } from './../styles/globalStyles';
 
 const AppHeader = () => (
-  <View style={GlobalStyles.AppHeader.container}>
-    <View style={{ flexDirection: "row" }}>
-      <TouchableOpacity style={GlobalStyles.Card.touchable}>
-        <Image
-          source={require("./../../../assets/images/logo32.png")}
-          style={[GlobalStyles.AppHeader.logoContainer]}
-        />
-      </TouchableOpacity>
+    <View style={GlobalStyles.AppHeader.container}>
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity style={GlobalStyles.Card.touchable}>
+                <Image
+                    source={require('./../../../assets/images/logo32.png')}
+                    style={[GlobalStyles.AppHeader.logoContainer]}
+                />
+            </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity style={GlobalStyles.Card.touchable}>
+                <IconFontAwesome name="camera-retro" size={32} />
+            </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity style={GlobalStyles.Card.touchable}>
+                <IconMaterialCommunityIcons
+                    name="message-text-outline"
+                    size={32}
+                />
+                <Badge style={GlobalStyles.AppHeader.badge}>69</Badge>
+            </TouchableOpacity>
+            <TouchableOpacity style={GlobalStyles.Card.touchable}>
+                <IconMaterialCommunityIcons name="login" size={32} />
+            </TouchableOpacity>
+        </View>
     </View>
-    <View style={{ flexDirection: "row" }}>
-      <TouchableOpacity style={GlobalStyles.Card.touchable}>
-        <IconFontAwesome name="camera-retro" size={32} />
-      </TouchableOpacity>
-    </View>
-    <View style={{ flexDirection: "row" }}>
-      <TouchableOpacity style={GlobalStyles.Card.touchable}>
-        <IconMaterialCommunityIcons name="message-text-outline" size={32} />
-        <Badge style={GlobalStyles.AppHeader.badge}>69</Badge>
-      </TouchableOpacity>
-      <TouchableOpacity style={GlobalStyles.Card.touchable}>
-        <IconMaterialCommunityIcons name="login" size={32} />
-      </TouchableOpacity>
-    </View>
-  </View>
 );
 
 export default AppHeader;
--- 1x24mobile/src/ui/components/feed.js
+++ 1x24mobile/src/ui/components/feed.js
@@ -1,72 +1,72 @@
-import * as React from "react";
-import { View, ScrollView } from "react-native";
-import PagerView from "react-native-pager-view";
-import FeedCard from "./feedCard";
-import { GlobalStyles } from "./../styles/globalStyles";
+import * as React from 'react';
+import { View, ScrollView } from 'react-native';
+import PagerView from 'react-native-pager-view';
+import FeedCard from './feedCard';
+import { GlobalStyles } from './../styles/globalStyles';
 
 const Feed = () => (
-  <PagerView style={GlobalStyles.Feed.container} initialPage={0}>
-    <View style={GlobalStyles.Card.container} key="1">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="2">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="3">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="4">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="5">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="6">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="7">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="8">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="9">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="10">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="11">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="12">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="13">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="14">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="15">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="16">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="17">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="18">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="19">
-      <FeedCard />
-    </View>
-    <View style={GlobalStyles.Card.container} key="20">
-      <FeedCard />
-    </View>
-  </PagerView>
+    <PagerView style={GlobalStyles.Feed.container} initialPage={0}>
+        <View style={GlobalStyles.Card.container} key="1">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="2">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="3">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="4">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="5">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="6">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="7">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="8">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="9">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="10">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="11">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="12">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="13">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="14">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="15">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="16">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="17">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="18">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="19">
+            <FeedCard />
+        </View>
+        <View style={GlobalStyles.Card.container} key="20">
+            <FeedCard />
+        </View>
+    </PagerView>
 );
 
 export default Feed;
