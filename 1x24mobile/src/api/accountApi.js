ac7a91afbf99464f6692b7fbb18e7e2616d47be4
Chris Wilson <chris@ocmit.com>
2024-04-04 13:47:10 -0700

    Updated accountApi.js, refactored apiService.js, and improved jwtService.js
    Updated `accountApi.js` to import `JwtService` and added `loginUser` function for posting user credentials to `/account/login` and setting the returned token. Refactored `apiService.js` by replacing `axios` with `fetch` API, removing several functions, and adding `postData` function for posting data and returning JSON response with error handling. Improved `jwtService.js` by changing string literals to single quotes, updating `jwtRefreshToken` to return an empty string for undefined, null, or empty tokens, and modifying `isJwtTokenExpired` to return true for token parsing errors.

--- 1x24mobile/src/api/accountApi.js
+++ 1x24mobile/src/api/accountApi.js
@@ -1,1 +1,14 @@
-import * as ApiService from "./apiService";
+import * as ApiService from './apiService';
+import * as JwtService from './jwtService';
+
+export const loginUser = (username, password) => {
+    const apiURL = '/account/login';
+    const dataToPost = {
+        username: username,
+        password: password
+    };
+
+    ApiService.postData(apiURL, dataToPost, 'POST').then((data) => {
+        JwtService.jwtSetToken(data.token);
+    });
+};
