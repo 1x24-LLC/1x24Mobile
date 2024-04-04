ab42a80576c83ea8ddbf33a7baac192dd303b1bc
Chris Wilson <chris@ocmit.com>
2024-04-04 10:05:02 -0700

    Added expo-secure-store and JWT service, updated API_BASE_URL
    Added the `expo-secure-store` plugin to `app.json` with Face ID permission, and as a dependency in `package.json` and `package-lock.json`. A new `jwtService.js` file was created to handle JWT tokens using `expo-secure-store`. The `API_BASE_URL` in `config.js` was updated for non-production environments.

---
+++ 1x24mobile/src/api/jwtService.js
@@ -0,0 +1,76 @@
+import * as SecureStore from "expo-secure-store";
+import { API_BASE_URL } from "/src/config";
+
+const jwtTokenName = "secureJwtToken";
+
+export const jwtSetToken = async (token) => {
+  try {
+    await SecureStore.setItemAsync(jwtTokenName, token);
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const jwtGetToken = async () => {
+  try {
+    let token = await SecureStore.getItemAsync(jwtTokenName);
+    if (isJwtTokenExpired(token)) {
+      token = jwtRefreshToken(token);
+    }
+    return token;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const jwtRefreshToken = async (token) => {
+  let url = API_BASE_URL & "/account/refresh";
+  let tokenValue = jwtGetToken();
+
+  if (isJwtTokenExpired(tokenValue)) {
+    console.log("Refreshing token");
+    let data = {
+      jwtToken: tokenValue,
+    };
+
+    return fetch(url, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        let newToken = json.token;
+        jwtSetToken(newToken);
+        return newToken;
+      })
+      .catch((error) => {
+        console.error("There was a problem refreshing token:", error);
+      });
+  }
+};
+
+export const isJwtTokenExpired = async (token) => {
+  try {
+    const bufferSeconds = 30;
+    const jwtPayload = JSON.parse(window.atob(token.split(".")[1]));
+    const jwtExpiration = jwtPayload.exp - bufferSeconds;
+    const timeNow = Math.floor(Date.now() / 1000);
+
+    return timeNow >= jwtExpiration;
+  } catch (error) {
+    return true;
+  }
+};
--- 1x24mobile/src/config.js
+++ 1x24mobile/src/config.js
@@ -3,7 +3,7 @@ let API_BASE_URL;
 if (process.env.NODE_ENV === "production") {
   API_BASE_URL = "https://api.1x24.com";
 } else {
-  API_BASE_URL = "https://1x24-test.azurewebsites.net/";
+  API_BASE_URL = "https://1x24-test.azurewebsites.net";
 }
 
 export { API_BASE_URL };
